deb961f73e7aa679f97a6a846c34066741f0b9df
AbdelBa <abdelhamid33@gmail.com>
2012-03-10 11:45:13 +0100

    Coloration JS opérationnelle, code simplifié à l'extrême

--- Controllers/JavaScriptHighlighter.cpp
+++ Controllers/JavaScriptHighlighter.cpp
@@ -4,110 +4,52 @@
  * aura son propre format (format mot-clé, format commentaire, ...).
  */
 
-#include <QtGui>
-
 #include "JavaScriptHighlighter.h"
 
-JavaScriptHighlighter::JavaScriptHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent)
+JavaScriptHighlighter::JavaScriptHighlighter(QTextDocument *parent) : Highlighter(parent)
 {
-    HighlightingRule rule;
+    // Les nombres.
+    numberFormat.setFontWeight(QFont::Bold);
+    numberFormat.setForeground(Qt::darkBlue);
+    setRule(JavaScriptData::numberRegex, numberFormat);
 
     // Les méthodes et fonctions.
     functionFormat.setFontWeight(QFont::Bold);
-    rule.pattern = JavaScriptData::functionRegex;
-    rule.format = functionFormat;
-    highlightingRules.append(rule);
+    setRule(JavaScriptData::functionRegex, functionFormat);
 
     // Les mots clé.
     keywordFormat.setForeground(Qt::darkBlue);
     keywordFormat.setFontWeight(QFont::Bold);
-    rule.pattern = JavaScriptData::keywordRegex;
-    rule.format = keywordFormat;
-    highlightingRules.append(rule);
+    setRule(JavaScriptData::keywordRegex, keywordFormat);
 
     // Les mots clé de déclaration.
     keywordDeclarationFormat.setFontWeight(QFont::Bold);
     keywordDeclarationFormat.setForeground(Qt::darkMagenta);
-    rule.pattern = JavaScriptData::keywordDeclarationRegex;
-    rule.format = keywordDeclarationFormat;
-    highlightingRules.append(rule);
+    setRule(JavaScriptData::keywordDeclarationRegex, keywordDeclarationFormat);
 
     // Les mots réservés.
     keywordReservedFormat.setFontWeight(QFont::Bold);
     keywordReservedFormat.setForeground(Qt::darkYellow);
-    rule.pattern = JavaScriptData::keywordReservedRegex;
-    rule.format = keywordReservedFormat;
-    highlightingRules.append(rule);
+    setRule(JavaScriptData::keywordReservedRegex, keywordReservedFormat);
 
     // Les fonctions internes de JavaScript.
     builtInFormat.setForeground(Qt::darkYellow);
-    rule.pattern = JavaScriptData::builtInRegex;
-    rule.format = builtInFormat;
-    highlightingRules.append(rule);
-
-    // Les identifiants.
-
-
-    // Les nombres.
-    numberFormat.setFontWeight(QFont::Bold);
-    numberFormat.setForeground(Qt::darkBlue);
-    rule.pattern = JavaScriptData::numberRegex;
-    highlightingRules.append(rule);
+    setRule(JavaScriptData::builtInRegex, builtInFormat);
 
     // Commentaire sur une seule ligne.
     singleLineCommentFormat.setForeground(Qt::red);
-    rule.pattern = JavaScriptData::singleLineCommentRegex;
-    rule.format = singleLineCommentFormat;
-    highlightingRules.append(rule);
-
-    // Commentaires multilignes.
-    multilineCommentFormat.setForeground(Qt::darkRed);
+    setRule(JavaScriptData::singleLineCommentRegex, singleLineCommentFormat);
 
     // Les simples et doubles quotes.
     quotationFormat.setForeground(Qt::darkGreen);
-    rule.pattern = JavaScriptData::quotationRegex;
-    rule.format = quotationFormat;
-    highlightingRules.append(rule);
+    setRule(JavaScriptData::quotationRegex, quotationFormat);
+
+    // Commentaires multilignes.
+    multilineCommentFormat.setForeground(Qt::darkRed);
+    setMultilineRule(JavaScriptData::multilineCommentStartRegex,
+                     JavaScriptData::multilineCommentEndRegex,
+                     multilineCommentFormat,
+                     IN_COMMENT_STATE);
 }
 
-void JavaScriptHighlighter::highlightBlock(const QString &text)
-{
-    foreach (const HighlightingRule &rule, highlightingRules)
-    {
-        QRegExp expression(rule.pattern);
-        int index = expression.indexIn(text);
-        while (index >= 0)
-        {
-            int length = expression.matchedLength();
-            setFormat(index, length, rule.format);
-            index = expression.indexIn(text, index + length);
-        }
-    }
 
-    setCurrentBlockState(DEFAULT_STATE);
-
-    int startIndex = 0;
-    if (previousBlockState() != IN_COMMENT_STATE)
-    {
-        startIndex = JavaScriptData::multilineCommentStartRegex.indexIn(text);
-    }
-
-    while (startIndex >= 0)
-    {
-        int endIndex = JavaScriptData::multilineCommentEndRegex.indexIn(text, startIndex);
-        int commentLength;
-
-        if (endIndex == -1)
-        {
-            setCurrentBlockState(IN_COMMENT_STATE);
-            commentLength = text.length() - startIndex;
-        }
-        else
-        {
-            commentLength = endIndex - startIndex + JavaScriptData::multilineCommentEndRegex.matchedLength();
-        }
-
-        setFormat(startIndex, commentLength, multilineCommentFormat);
-        startIndex = JavaScriptData::multilineCommentStartRegex.indexIn(text, startIndex + commentLength);
-    }
-}
--- Controllers/JavaScriptHighlighter.h
+++ Controllers/JavaScriptHighlighter.h
@@ -6,37 +6,21 @@
 #ifndef JavaScriptHighlighter_h
 #define JavaScriptHighlighter_h
 
-#include <QSyntaxHighlighter>
-#include <QHash>
-#include <QTextCharFormat>
-
 #include "../Models/JavaScriptData.h"
-
-#define DEFAULT_STATE 0     // L'état par défaut.
-#define IN_COMMENT_STATE 1  // En commentaires multilignes.
+#include "Highlighter.h"
 
 QT_BEGIN_NAMESPACE
 class QTextDocument;
 QT_END_NAMESPACE
 
-class JavaScriptHighlighter : public QSyntaxHighlighter
+class JavaScriptHighlighter : public Highlighter
 {
     Q_OBJECT
 
 public:
     JavaScriptHighlighter(QTextDocument *parent = 0);
 
-protected:
-    void highlightBlock(const QString &text);
-
 private:
-    struct HighlightingRule
-    {
-        QRegExp pattern;
-        QTextCharFormat format;
-    };
-    QVector<HighlightingRule> highlightingRules;
-
     QTextCharFormat functionFormat;
     QTextCharFormat keywordFormat;
     QTextCharFormat keywordDeclarationFormat;
