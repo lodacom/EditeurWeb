baec49dbe53e98783b9f51e6dced40cbd114b97b
AbdelBa <abdelhamid33@gmail.com>
2012-03-10 11:46:19 +0100

    Coloration Html bientôt finie, le JS et le CSS inclus pas encore en place.

--- Controllers/HtmlHighlighter.cpp
+++ Controllers/HtmlHighlighter.cpp
@@ -1,6 +1,49 @@
+/*
+ * Auteur : Abdelhamid
+ * Description : configuration des formats, chaque type de mot reconnaissable
+ * aura son propre format (format mot-clé, format commentaire, ...).
+ */
+
 #include "HtmlHighlighter.h"
 
-HtmlHighlighter::HtmlHighlighter(QObject *parent) :
-    QObject(parent)
+HtmlHighlighter::HtmlHighlighter(QTextDocument *parent) : Highlighter(parent)
 {
+    // Les nombres.
+    numberFormat.setFontWeight(QFont::Bold);
+    numberFormat.setForeground(Qt::darkBlue);
+    setRule(HtmlData::numberRegex, numberFormat);
+
+    // Les balises.
+    tagsFormat.setForeground(Qt::lightGray);
+    tagsFormat.setFontWeight(QFont::Bold);
+
+    setRule(HtmlData::tagOpenBeginRegex, tagsFormat);
+    setRule(HtmlData::tagOpenEndRegex, tagsFormat);
+    setRule(HtmlData::tagCloseRegex, tagsFormat);
+    setRule(HtmlData::singleTagCloseRegex, tagsFormat);
+    setRule(HtmlData::scriptTagOpenRegex, tagsFormat);
+    setRule(HtmlData::scriptTagCloseRegex, tagsFormat);
+    setRule(HtmlData::styleTagOpenRegex, tagsFormat);
+    setRule(HtmlData::styleTagCloseRegex, tagsFormat);
+
+    // Les attributs d'une balise.
+    tagAttributeFormat.setForeground(Qt::darkMagenta);
+    tagAttributeFormat.setFontWeight(QFont::Bold);
+    setRule(HtmlData::tagAttributeRegex, tagAttributeFormat);
+
+    // Les mots spéciaux (p.ex. &nbsp;).
+    specialCharFormat.setForeground(Qt::darkBlue);
+    specialCharFormat.setFontWeight(QFont::Bold);
+    setRule(HtmlData::specialCharRegex, specialCharFormat);
+
+    // Les simples et doubles quotes.
+    quotationFormat.setForeground(Qt::darkGreen);
+    setRule(HtmlData::quotationRegex, quotationFormat);
+
+    // Commentaires multilignes.
+    multilineCommentFormat.setForeground(Qt::darkRed);
+    setMultilineRule(HtmlData::multilineCommentStartRegex,
+                     HtmlData::multilineCommentEndRegex,
+                     multilineCommentFormat,
+                     IN_COMMENT_STATE);
 }
--- Controllers/HtmlHighlighter.h
+++ Controllers/HtmlHighlighter.h
@@ -1,18 +1,45 @@
-#ifndef HTMLHIGHLIGHTER_H
-#define HTMLHIGHLIGHTER_H
+/*
+ * Auteur : Abdelhamid
+ * Description : couleurs et formats à appliquer au code Html.
+ * le JavaScript et Css inclus sont pris en charge par les classes JavaScriptHighlighet et CssHighlighter.
+ */
 
-#include <QObject>
+#ifndef HtmlHighlighter_h
+#define HtmlHighlighter_h
 
-class HtmlHighlighter : public QObject
+#include <QSyntaxHighlighter>
+#include <QTextCharFormat>
+
+#include "Highlighter.h"
+#include "../Models/HtmlData.h"
+
+#define IN_SCRIPT_TAG_STATE 2   // En mode JavaScript.
+#define IN_STYLE_TAG_STATE 3    // En mode Css.
+
+
+QT_BEGIN_NAMESPACE
+class QTextDocument;
+QT_END_NAMESPACE
+
+class HtmlHighlighter : public Highlighter
 {
     Q_OBJECT
+
 public:
-    explicit HtmlHighlighter(QObject *parent = 0);
-    
-signals:
-    
-public slots:
-    
+    HtmlHighlighter(QTextDocument *parent = 0);
+
+protected:
+    //void highlightBlock(const QString &text);
+
+private:
+    QTextCharFormat numberFormat;
+    QTextCharFormat quotationFormat;
+    QTextCharFormat specialCharFormat;
+    QTextCharFormat multilineCommentFormat;
+
+    // Les balises ouvrantes, fermantes, script et style seront formatées à l'identique.
+    QTextCharFormat tagsFormat;
+    QTextCharFormat tagAttributeFormat;
 };
 
-#endif // HTMLHIGHLIGHTER_H
+#endif
