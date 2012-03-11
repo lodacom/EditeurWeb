ec28881cb5164b6334a4981829dfa8041d4bfa7a
burger <burger@burgerfactory.(none)>
2012-03-11 14:59:12 +0100

    Ajout des modifications nécessaires à la classification par ordre alphabétique

--- Models/Element.h
+++ Models/Element.h
@@ -6,9 +6,15 @@ using namespace std;
 
 class Element{
 //Opérateur permettant le tri. 
-friend bool operator < (const Element & lElem, const Element & rElem)
-{
-	 return (lElem.name < rElem.name ? true : false);
+friend bool operator < (const Element & lElem, const Element & rElem){
+	//Création de variables temporaires, nécessaire pour le tri par ordre aplhabétique
+	string temp1 = lElem.name;
+	string temp2 = rElem.name;
+	//On "transforme" les noms des éléments de l'arborescence en minuscules, pour la comparaison
+	//Il s'agit d'un parcours de string transformant caractère par caractère
+	std::transform(temp1.begin(), temp1.end(), temp1.begin(), ::tolower);
+	std::transform(temp2.begin(), temp2.end(), temp2.begin(), ::tolower);
+	return (temp1 < temp2 ? true : false);
 }
 public:
     //Constructeur Destructeur
