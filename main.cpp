3e5a569ba0f4755e4e983aad47739761f3db6bf2
Jo <jo@Shad-Pc.(none)>
2012-04-10 14:22:45 +0200

    Rajout de l'action "new project" dans la barre de menu

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ int main(int argc, char *argv[])
     window.showMaximized();
 
     /* Provisoire, test de l'arborescence. Signé : Abdel */
-    Q_INIT_RESOURCE(examples);
+    /*Q_INIT_RESOURCE(examples);
     QFile file(":/Examples/jocrisse.html");
     file.open(QIODevice::ReadOnly);
     SyntaxTree model(file.readAll());
     file.close();
-
-    QTreeView view;
-    view.setModel(&model);
-    view.setWindowTitle(QObject::tr("Simple Tree Model"));
-    view.show();
+    */
+    //QTreeView view;
+    //view.setModel(&model);
+    //view.setWindowTitle(QObject::tr("Simple Tree Model"));
+    //view.show();
 
     return app.exec();
 }
