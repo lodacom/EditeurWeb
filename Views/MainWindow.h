ab14c526cb5c0d24cd480834e7c26aa5077a5687
lodacom <loda-compagnie@hotmail.fr>
2012-04-23 23:09:50 +0200

    Commentaires ajoutés pour la génération de la documentation

--- Views/MainWindow.h
+++ Views/MainWindow.h
@@ -1,3 +1,8 @@
+/*!
+ * \file MainWindow.h
+ * \authors Olivier Mickael Joachim
+ * \version 0.2
+ */
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -23,29 +28,100 @@ class QWebView;
 class QTreeWidget;
 QT_END_NAMESPACE
 
+/*!
+* \class MainWindow
+* \brief Permet d'obtenir toute l'interface utilisateur avec son
+* menu, son dock, et ses actions correspondantes.
+*/
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \brief Constructeur
+     * Création de la fenêtre principale contenant toute l'interface.
+     * \param parent: la fenêtre concernée par toutes les actions de
+     * l'utilisateur.
+     */
     MainWindow(QWidget *parent = 0);
-    MainWindow(const QString &fileName);
+    //MainWindow(const QString &fileName);
 
 public slots:
+    /*!
+     * \brief Permet d'ouvrir un fichier dans un nouvel onglet
+     */
     void open();
+
+    /*!
+     * \brief Permet de sauvegarder l'onglet actif
+     */
     void save();
+
+    /*!
+     * \brief Permet de sauvegarder en tant que l'onglet actif
+     */
     void saveAs();
+
+    /*!
+     * \brief Enclenche l'action couper sur l'onglet courant
+     */
     void cut();
+
+    /*!
+     * \brief Enclenche l'action copier sur l'onglet courant
+     */
     void copy();
+
+    /*!
+     * \brief Enclenche l'action coller sur l'onglet courant
+     */
     void paste();
+
+    /*!
+     * \brief Enclenche l'action annuler sur l'onglet courant
+     */
     void undo();
+
+    /*!
+     * \brief Enclenche l'action refaire sur l'onglet courant
+     */
     void redo();
+
+    /*!
+     * \brief Enclenche l'action tout sélectionner sur l'onglet courant
+     */
     void selectAll();
+
+    /*!
+     */
     void updateWindowMenu();
+
+    /*!
+     * \brief Met à jour la bar de menu en fonction de la
+     * présence ou non de fenêtre(s) au centre de l'interface.
+     */
     void updateMenus();
+
+    /*!
+     * \brief Crée un fils de type CentralEditor et l'ajoute au centre
+     * de l'interface.
+     * \return Un onglet de type CentralEditor.
+     */
     CentralEditor *createMdiChild();
+
+    /*!
+     * \brief Met à "vrai" la fenêtre qui a été modifiée et indique
+     * ainsi que le document concerné l'est également
+     */
     void documentWasModified();
+
+    /*!
+     * \brief Change la façon d'afficher pour les fenêtres.
+     */
     void switchLayoutDirection();
+
+
     void setActiveSubWindow(QWidget *window);
     void about();
     void newFile();
@@ -57,7 +133,6 @@ protected:
 
 private:
     void init();
-    //void setupColoration();
     void setupWorkSpaceDock();
     //void setupHtmlDock();
 
