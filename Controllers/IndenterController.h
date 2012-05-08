5694308592c4184bcfe004615348f65bb93b2679
Jo <jo@Shad-Pc.(none)>
2012-05-08 11:54:33 +0200

    Ajout des commentaires pour la classe IndenterController

--- Controllers/IndenterController.h
+++ Controllers/IndenterController.h
@@ -11,13 +11,33 @@
 #define CSS_LANGUAGE 1
 #define JS_LANGUAGE 2
 #define PHP_LANGUAGE 3
+/*!
+  *\class IndenterController
+  *\brief Classe controleur présente dans les central Editor, elle fait le lien avec les différents indenteurs.
+  */
 class IndenterController:public QObject{
 Q_OBJECT
 
 public:
+    /*!
+      *\brief Constructeur
+      */
     IndenterController();
+    /*!
+      *\brief Destructeur
+      */
     virtual ~IndenterController();
+    /*!
+      *\brief Méthode permettant selon la ligne passée en paramètre de savoir si l'on indente ou non.
+      *\param line: ligne passée en paramètre
+      *\return retourne -1 si il faut enlever une tabulation sur la ligne actuelle,
+      * 1 s'il faut en rajouter une à la suivante, et 0 lorsqu'on doit laissé l'indentation telle qu'elle.
+      */
     virtual int indentDetermin(QString line);
+    /*!
+      *\brief Méthode permettant de spécifier au controleur le langage courant.
+      *\param language: entier définissant le language, correspondant à une des constantes définies dans cette classe.
+      */
     virtual void setLanguage(int language);
 private:
     HtmlIndenter *htmlIndenter;
